11124c6d75ac1dc57b1dc94b823097ef3b8c4b39
Conan Tzou <conan@asperasoft.com>
2016-04-26 19:42:56 -0700

    register subtasks and callbacks

--- plug.js
+++ plug.js
@@ -6,14 +6,26 @@ module.exports = plug;
 
 var tasks = {};
 
-function onTask (name, callback) {
-  // register tasks
-  tasks[name] = callback;
+function onTask (name) {
+  // register tasks and subtasks
+  if (Array.isArray(arguments[1]) && typeof arguments[2] === 'function') {
+    tasks[name] = {
+      subTasks: arguments[1],
+      callback: arguments[2]
+    };
+  } else if (typeof arguments[1] === 'function') {
+    tasks[name] = {
+      subTasks: [],
+      callback: arguments[1]
+    };
+  } else {
+    console.log('Invalid task registration');
+  }
 }
 
 process.nextTick(() => {
   var taskName = process.argv[2];
-  
+
   if (taskName && tasks[taskName]) {
     tasks[taskName]();
   } else {
