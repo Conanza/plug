7327f1b057cd39cbe37ca2083e7ccca279104959
Conan Tzou <conan@asperasoft.com>
2016-04-26 19:47:30 -0700

    run subtasks

--- plug.js
+++ plug.js
@@ -23,11 +23,23 @@ function onTask (name) {
   }
 }
 
+function runTask (name) {
+  if (tasks[name].subTasks) {
+    tasks[name].subTasks.forEach(subTaskName => {
+      runTask(subTaskName);
+    });
+  }
+
+  if (tasks[name].callback) {
+    tasks[name].callback();
+  }
+}
+
 process.nextTick(() => {
   var taskName = process.argv[2];
 
   if (taskName && tasks[taskName]) {
-    tasks[taskName]();
+    runTask(taskName);
   } else {
     console.log('Unknown task', taskName);
   }
